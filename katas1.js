b808e995790fd7725fde8b6033d94fc5dcfc91f9
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-10-23 19:36:13 +0000

    First attempt!

---
+++ katas1.js
@@ -0,0 +1,8 @@
+const sumLargestNumbers = function(data){
+  const newNums = data.sort(function(a, b){return b - a})
+  return newNums[0] + newNums [1]
+}
+
+console.log(sumLargestNumbers([1,10]))
+console.log(sumLargestNumbers([1,2,3]))
+console.log(sumLargestNumbers([10,4,34,6,92,2]))
